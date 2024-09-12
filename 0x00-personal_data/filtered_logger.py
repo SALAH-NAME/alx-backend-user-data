bb840a9360ee73ee73ae975a2ed6e6a7879c291d
Salah <Salah>
2024-09-12 11:05:09 +0100

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,69 +1,17 @@
 #!/usr/bin/env python3
 """ filtered_logger.py """
-from typing import List
 import re
+from typing import List
 import logging
-from os import environ
 import mysql.connector
+import os
 
 
-PERSONAL_INFO_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-def obfuscate_log_message(fields: List[str], redaction: str,
-                          log_message: str, separator: str) -> str:
-    """obfuscate_log_message function"""
-    for field in fields:
-        log_message = re.sub(f'{field}=.*?{separator}',
-                             f'{field}={redaction}{separator}', log_message)
-    return log_message
-
-
-def create_logger() -> logging.Logger:
-    """create_logger function"""
-    user_logger = logging.getLogger("user_data")
-    user_logger.setLevel(logging.INFO)
-    user_logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(list(PERSONAL_INFO_FIELDS)))
-    user_logger.addHandler(stream_handler)
-    return user_logger
-
-
-def connect_to_db() -> mysql.connector.connection.MySQLConnection:
-    """connect_to_db function"""
-    db_username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    db_password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-
-    connection = mysql.connector.connection.MySQLConnection(
-            user=db_username, password=db_password,
-            host=db_host, database=db_name)
-    return connection
-
-
-def main():
-    """main function"""
-    database = connect_to_db()
-    db_cursor = database.cursor()
-    db_cursor.execute("SELECT * FROM users;")
-    column_names = [column[0] for column in db_cursor.description]
-
-    user_logger = create_logger()
-
-    for row in db_cursor:
-        formatted_row = ''.join(f'{column}={str(value)}; '
-                                for value, column in zip(row, column_names))
-        user_logger.info(formatted_row.strip())
-
-    db_cursor.close()
-    database.close()
+PERSONAL_INFO_FIELDS = ("name", "email", "password", "ssn", "phone")
 
 
 class RedactingFormatter(logging.Formatter):
     """RedactingFormatter function"""
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
@@ -75,10 +23,65 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """format function"""
-        record.msg = obfuscate_log_message(self.fields, self.REDACTION,
-                                           record.getMessage(), self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+        return obfuscate_log_message(self.fields, self.REDACTION,
+                                     super().format(record), self.SEPARATOR)
+
+
+def connect_to_db() -> mysql.connector.connection.MYSQLConnection:
+    """connect_to_db function"""
+    db_connection = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return db_connection
+
+
+def obfuscate_log_message(fields: List[str], redaction: str, log_message: str,
+                          separator: str) -> str:
+    """obfuscate_log_message function"""
+    for field in fields:
+        log_message = re.sub(f'{field}=(.*?){separator}',
+                             f'{field}={redaction}{separator}', log_message)
+    return log_message
+
+
+def create_logger() -> logging.Logger:
+    """create_logger function"""
+    user_logger = logging.getLogger("user_data")
+    user_logger.setLevel(logging.INFO)
+    user_logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+
+    formatter = RedactingFormatter(list(PERSONAL_INFO_FIELDS))
+    stream_handler.setFormatter(formatter)
+
+    user_logger.addHandler(stream_handler)
+    return user_logger
+
+
+def main() -> None:
+    """main function"""
+    database = connect_to_db()
+    db_cursor = database.cursor()
+    db_cursor.execute("SELECT * FROM users;")
+
+    column_names = [column[0] for column in db_cursor.description]
+    user_logger = create_logger()
+
+    for row in db_cursor:
+        formatted_row = ''
+        for value, column in zip(row, column_names):
+            formatted_row += f'{column}={(value)}; '
+        user_logger.info(formatted_row)
+
+    db_cursor.close()
+    database.close()
 
 
 if __name__ == '__main__':
+    """__main__"""
     main()
