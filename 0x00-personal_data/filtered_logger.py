88911647e2f730095b22251dfe477e189450d2bd
Salah <Salah>
2024-09-12 09:58:10 +0100

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,96 +1,94 @@
 #!/usr/bin/env python3
 """ filtered_logger.py """
+from typing import List
 import re
 import logging
+from os import environ
 import mysql.connector
-import os
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+SENSITIVE_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields, redaction, message, separator):
-    """filter_datum function
+def mask_data(fields: List[str], mask: str,
+              log_message: str, delimiter: str) -> str:
+    """mask_data function
     """
-    pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
-    return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={redaction}",
-                  message)
+    for field in fields:
+        log_message = re.sub(f'{field}=.*?{delimiter}',
+                             f'{field}={mask}{delimiter}', log_message)
+    return log_message
 
 
-class RedactingFormatter(logging.Formatter):
-    """RedactingFormatter class
+def create_logger() -> logging.Logger:
+    """create_logger function
     """
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
 
-    def __init__(self, fields):
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(MaskingFormatter(list(SENSITIVE_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def connect_db() -> mysql.connector.connection.MySQLConnection:
+    """connect_db function
+    """
+    db_user = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    db_password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    connection = mysql.connector.connection.MySQLConnection(
+        user=db_user, password=db_password,
+        host=db_host, database=db_name)
+    return connection
+
+
+class MaskingFormatter(logging.Formatter):
+    """MaskingFormatter Class"""
+
+    MASK = "***"
+    LOG_FORMAT = ("[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: "
+                  "%(message)s")
+    DELIMITER = ";"
+
+    def __init__(self, fields: List[str]):
         """__init__
         """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super(MaskingFormatter, self).__init__(self.LOG_FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """format function
         """
-        original_message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, original_message,
-                            self.SEPARATOR)
-
-
-def get_logger() -> logging.Logger:
-    """get_logger function
-    """
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(fields=PII_FIELDS)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-    return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """get_db function
-    """
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-
-    return mysql.connector.connect(
-        user=username,
-        password=password,
-        host=host,
-        database=db_name
-    )
+        record.msg = mask_data(self.fields, self.MASK,
+                               record.getMessage(), self.DELIMITER)
+        return super(MaskingFormatter, self).format(record)
 
 
 def main():
     """main function
     """
-    db = get_db()
+    db = connect_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
-    logger = get_logger()
+    column_names = [i[0] for i in cursor.description]
+
+    logger = create_logger()
 
     for row in cursor:
-        message = (
-            f"name={row[0]}; email={row[1]}; phone={row[2]}; ssn={row[3]}; "
-            f"password={row[4]}; ip={row[5]}; last_login={row[6]}; "
-            f"user_agent={row[7]};"
-        )
-        log_record = logging.LogRecord(
-            "user_data", logging.INFO, None, None, message, None, None
-        )
-        logger.handle(log_record)
+        log_message = ''.join(f'{column}={str(value)}; '
+                              for value, column in zip(row, column_names))
+        logger.info(log_message.strip())
 
     cursor.close()
     db.close()
 
 
-if __name__ == "__main__":
-    """__main__
-    """
+if __name__ == '__main__':
+    """__main__"""
     main()
