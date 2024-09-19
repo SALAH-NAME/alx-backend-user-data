375bd898ba2d884f478f8bfd940301ce3c804cb1
Salah <Salah>
2024-09-19 21:01:01 +0100

    session_authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" session_auth.py """
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """login function
+    """
+    user_email = request.form.get('email')
+
+    if not user_email:
+        return jsonify({"error": "email missing"}), 400
+
+    user_password = request.form.get('password')
+
+    if not user_password:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        found_users = User.search({'email': user_email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not found_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in found_users:
+        if not user.is_valid_password(user_password):
+            return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import authentication
+
+    authenticated_user = found_users[0]
+    session_id = authentication.create_session(authenticated_user.id)
+
+    session_name = getenv("SESSION_NAME")
+
+    response = jsonify(authenticated_user.to_json())
+    response.set_cookie(session_name, session_id)
+
+    return response
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -130,7 +130,7 @@ def get_me() -> str:
     """
     if request.current_user is None:
         abort(404)
-    return jsonify(request.current_user.to_dict())
+    return jsonify(request.current_user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
@@ -142,9 +142,9 @@ def get_user(user_id: str) -> str:
     if user_id == "me":
         if request.current_user is None:
             abort(404)
-        return jsonify(request.current_user.to_dict())
+        return jsonify(request.current_user.to_json())
 
     user = User.get(user_id)
     if user is None:
         abort(404)
-    return jsonify(user.to_dict())
+    return jsonify(user.to_json())
